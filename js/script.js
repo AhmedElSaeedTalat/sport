45a8a9455320244a7b1ae2b5377dd1fd67ef1284
Ahmed Talat <ahmedelsaeed105@gmail.com>
2017-09-05 22:45:05 +0300

    =header

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$("#v1").click(function(){
+	 value = $("#v1").text();
+	$("#value1").html(value);
+});
+$("#v2").click(function(){
+	 value = $("#v2").text();
+	$("#value1").html(value);
+});
+$("#v3").click(function(){
+	 value = $("#v3").text();
+	$("#value1").html(value);
+});
